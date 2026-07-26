a482ce990c075f6c7e6651df7dc44a20e5d0ac9e
lucavallini <lucavallini1508@gmail.com>
2026-07-26 14:54:36 -0300

    cors netlify

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="AIBAR SRL - API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:4200",
-                   "https://aibar-frontend.vercel.app"],
+                   "https://aibarsrl.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
